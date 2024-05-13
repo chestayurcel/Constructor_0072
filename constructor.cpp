3e01fca5e28d79a51a65bad60c552774261abcf7
Yurcell <chestayurcel@gmail.com>
2024-05-13 16:01:04 +0700

    membuat constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+Mahasiswa::Mahasiswa(string iNama){
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+{
+    nim = iNim;
+    nama = iNama;
+};
+
+void Mahasiswa::cetak(){
+    cout << "NIM    = " << nim << endl;
+    cout << "Nama   = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Nopal");
+    Mahasiswa mhs4(30, "Neo");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
