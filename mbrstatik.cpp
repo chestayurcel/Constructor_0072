422f4f6be0b2ed22f1aa596f32d70896b536e7dc
Yurcell <chestayurcel@gmail.com>
2024-05-13 14:53:54 +0700

    menambahkan konstruktor untuk menginisialisasi anggota nama dan memanggil metode setID di kelas mahasiswa

--- mbrstatik.cpp
+++ mbrstatik.cpp
@@ -8,3 +8,12 @@ public:
     static int nim;
     int id;
     string nama;
+    
+    void setID();
+
+    void printAll();
+    mahasiswa(string pnama):nama(pnama)
+    {
+        setID;
+    }
+};
